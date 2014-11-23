a1526f1fce1a92e5fa8bcfd87b245a72c5eb3dff
U-Uranami\Miura Hareaki <miurahareaki@gmail.com>
2014-11-23 15:05:43 +0100

    CGameMakerLoad should be ok, for real

--- Jeu/SmallWorld/ISaveManager.cs
+++ Jeu/SmallWorld/ISaveManager.cs
@@ -6,8 +6,8 @@ namespace SmallWorld
         Tuple<int, int, int> getGameState();
         Tuple<int, int, System.Collections.Generic.List<int>> getMapData();
         Tuple<string, string, int> getPlayerData();
-        System.Collections.Generic.List<Unit> getUnitsData(int numPlayer);
+        System.Collections.Generic.List<Unit> getUnits(int numPlayer);
         void load();
-        void save()
+        void save();
     }
 }
